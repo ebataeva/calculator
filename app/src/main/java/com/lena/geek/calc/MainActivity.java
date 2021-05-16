8d05164fa4607f1e9cb45419a937d5e926e4ce0b
ebataeva <ebataeva@gmail.com>
2021-05-16 15:51:51 +0300

    пустой проект под калькулятор

---
+++ app/src/main/java/com/lena/geek/calc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lena.geek.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
